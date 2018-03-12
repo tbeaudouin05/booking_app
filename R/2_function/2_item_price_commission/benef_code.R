8122381edab2b04a9568a4ae1f29985ec4f0ac7d
tbeaudouin05 <tbeaudouin03@gmail.com>
2018-03-12 15:24:26 +0330

    v_1_0_5: no replicate seller name benef_code_map + improved query_without_error

--- R/2_function/2_item_price_commission/benef_code.R
+++ R/2_function/2_item_price_commission/benef_code.R
@@ -1,8 +1,9 @@
 benef_code_f <- function(df_to_map, df_to_map_name,loop_number){
   
   i <- 0
+  df_with_benef_code <- NULL
   
-  while (i == 0) {
+  while (i < 2) {
     
     # close all Excel workbooks to avoid errors
     for (wb in xl.workbooks()) {xl.workbook.close(wb)}
@@ -20,6 +21,11 @@ benef_code_f <- function(df_to_map, df_to_map_name,loop_number){
     # to check there is no blanck in benef_code
     check_blank_benef_code_map <- benef_code_map[is.na(benef_code_map[,'benef_code']),]
     
+    # to check there are no exact duplicated seller names
+    duplicated_seller_name <- data.frame('seller_name' = benef_code_map$seller_name, 'is_duplicate' = duplicated(benef_code_map$seller_name))
+    duplicated_seller_name <- duplicated_seller_name[duplicated_seller_name[,'is_duplicate'] == 'TRUE',]
+
+    
     # if there are blank benef_code then re_run the loop and prompt user to rectify
     if (nrow(check_blank_benef_code_map) >0 ){
       
@@ -29,7 +35,29 @@ benef_code_f <- function(df_to_map, df_to_map_name,loop_number){
       
       tkmessageBox(title = paste('Blank benef_code!') 
                    , message = 'There are blank benef_codes in benef_code map - provide all benef_code THEN click OK'
-                   , type = 'ok')} else {
+                   , type = 'ok')
+      
+      # restart checking if there was any error in input
+      i <- 0
+      
+    } else if (nrow(duplicated_seller_name) > 0) {
+      
+      df_with_benef_code <- 're_run'
+      
+      write_excel_csv(duplicated_seller_name,'4_temp_input/duplicate_seller_name_map_missing.csv')
+      
+      shell.exec(paste0(getwd(),'/2_input/benef_code_map.xlsm'))
+      
+      shell.exec(paste0(getwd(),'/4_temp_input/duplicate_seller_name_map_missing.csv'))
+      
+      tkmessageBox(title = paste('Duplicated seller names!') 
+                   , message = 'There are duplicated seller names in benef_code map - remove all duplicates THEN click OK'
+                   , type = 'ok')
+      
+      # restart checking if there was any error in input
+      i <- 0
+      
+    } else {
     
     # map beneficiary codes -------------------------------------------------------------------------------
     df_with_benef_code <- user_interaction_update_input_f(df_to_map
@@ -42,10 +70,10 @@ benef_code_f <- function(df_to_map, df_to_map_name,loop_number){
                                                 ,loop_number)}
     
     
-    
-    
-    if (is.data.frame(df_with_benef_code)) { 
-      i <- 1
+      if (is.data.frame(df_with_benef_code)) { 
+      
+      # break the loop after two fully successful run - runs twice to check that user interaction did not input crap into the benef_map
+      i <- i + 1
       
       # reorder columns
       df_with_benef_code <- df_with_benef_code[,c(ncol(df_with_benef_code),1:ncol(df_with_benef_code)-1)]
--- R/2_function/run_query_without_error.R
+++ R/2_function/run_query_without_error.R
@@ -2,72 +2,77 @@
 run_query_wo_error_f <- function(formatted_query, is_sc_query = T){
   
   db_access <- yaml.load_file('SQL/db_access.yaml')
-  j <- 0
-  k <- 0
-  conn <- NULL
-  while (is.null(conn) & j < 10) {
-    
-  if (is_sc_query) {
-    
-    
-    writeLines('Connecting to Seller Center database...')
-  # connect to sc database
-  tryCatch({conn = dbConnect(MySQL(), user= db_access$sc_access[[1]][[1]]
-                                       , password= db_access$sc_access[[2]][[1]]
-                                       , dbname= db_access$sc_access[[3]][[1]]
-                                       , host= db_access$sc_access[[4]][[1]])}
-           ,error = function(cond){conn <- NULL})
-    
-  
-  } else {
-    writeLines('Connecting to OMS database...')
-    # connect to oms database
-    
-    tryCatch({conn = dbConnect(MySQL(), user= db_access$oms_access[[1]][[1]]
-                               , password= db_access$oms_access[[2]][[1]]
-                               , dbname= db_access$oms_access[[3]][[1]]
-                               , host= db_access$oms_access[[4]][[1]])}
-             ,error = function(cond){conn <- NULL})
-  }
-    
-    j <- j + 1
-  }
-  
+  j <- 1
   i <- 1
-  j <- 0
+  conn <- NULL
+  rs <- NULL
+  list <- list(i,data.frame())
   
+  # fetch correct credential for SC or OMS
+  if (is_sc_query) {
+    user1 <- db_access$sc_access[[1]][[1]]
+    password1 <- db_access$sc_access[[2]][[1]]
+    dbname1 <- db_access$sc_access[[3]][[1]]
+    host1 <- db_access$sc_access[[4]][[1]] 
+  } else {
+    user1 <- db_access$oms_access[[1]][[1]]
+    password1 <- db_access$oms_access[[2]][[1]]
+    dbname1 <- db_access$oms_access[[3]][[1]]
+    host1 <- db_access$oms_access[[4]][[1]]
+  }
+  
+  # re-run the query until all errors /  warnings have been handled
   while (i == 1 & j < 10) {
     
-  # define fetch_row_f function
-  fetch_row_f <- function(rs,conn1) {
-    # run formatted_sc_query on MySQL database
-    rs = dbSendQuery(conn1, formatted_query)
-    # fetch rows returned by the query into R
-    output = dbFetch(rs, n=-1)
-  return(output)}
+    writeLines('Querying database and handling potential errors, please wait...')
+    writeLines(paste('Try:',j))
+    
+    # tryCatch handles errors and warnings
+    # NB: the result of tryCatch is applied to i: 
+    # if there is an error / warning, then this tryCatch returns 1 (which continues the loop)
+    # if there is no error / warning, then the entire function run_query_wo_error_f returns query_output :) because tryCatch contains "return" hence we do not care about the value of i anymore anyway :)
+    i <- tryCatch({
+      
+    # establish connection to database
+    conn = dbConnect(MySQL(), user= user1, password= password1, dbname= dbname1, host= host1)
+    
+    
+    # run formatted_query on MySQL database
+    # if warnings do NOT contain "lost connection" AND do NOT contain "row" THEN delete warnings
+    # i.e. show warnings if and ONLY if warnings contain "lost connection" or "row"
+    query_output <- withCallingHandlers(dbGetQuery(conn, formatted_query, n = -1)
+                                        ,warning = function(w){if (!grepl('lost connection',w$message) & !grepl('row',w$message))
+                                          {invokeRestart('muffleWarning')}})
+    
+    # disconnect all database connections in case of success
+    lapply(dbListConnections(dbDriver(drv = "MySQL")), dbDisconnect)
+    
+    # if everything runs fine: break the loop and return query output
+    i <- 2
+    return(query_output)
+    
+    }
+    
+    # handle blocking warnings: re-run the loop 
+    # NB: all non blocking warnings should be suppressed thanks to withCallingHandlers function
+    ,warning = function(w){
+                          writeLines(paste('WARNING:',w$message))
+                          return(1)
+    }
+      # handle errors: re-run the loop
+    ,error = function(e){
+                          writeLines(paste('ERROR:',e$message))
+                          return(1)})
 
-  writeLines('Running query and handling potential errors, please wait...')
-  query_output <- withCallingHandlers(fetch_row_f(rs,conn),
-                                      warning = function(w){
-                                        if(grepl("error while fetching rows", w$message)){
-                                          k <- 0
-                                          writeLines(paste('Error while fetching rows',w$message))
-                                        } else if (grepl("MySQL server has gone away", w$message)) {
-                                          k <- 0
-                                          writeLines(paste('MySQL server has gone away: ',w$message))
-                                        } else {
-                                          writeLines('All good on my side - moving on and retrieving data! :)')
-                                          k <- 1
-                                        }
-                                      })
+
+  # disconnect all database connections in case of errors / warnings
+  lapply(dbListConnections(dbDriver(drv = "MySQL")), dbDisconnect)
   
-# break the loop when all known errors have been handled 
-if(is.data.frame(query_output) & k <- 1){i <- 2} else {i <- 1}
-  
-  lapply( dbListConnections( dbDriver( drv = "MySQL")), dbDisconnect)
+  # make sure it is not an infinite loop
+  j <- j + 1
+  # wait 3 seconds before re-running the loop
+  Sys.sleep(10)
   
   }
-  
-  return(query_output)
   
 }
