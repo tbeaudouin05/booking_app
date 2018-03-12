7f3ef1b1c909b929edcc896487eb842216170d78
tbeaudouin05 <tbeaudouin03@gmail.com>
2018-03-12 16:39:43 +0330

    v_1_0_7: batch timestamped log

--- do_not_move_me.bat
+++ do_not_move_me.bat
@@ -9,9 +9,21 @@ set str=%str:\=!word!%
 
 TASKKILL /F /IM EXCEL.exe
 
+ECHO -
+ECHO -
+ECHO -
+ECHO R is running - please wait until this black window disappears 
+ECHO R can take up to 1 hour depending on the number of transactions booked
+ECHO -
+ECHO You can also check the log folder to check the program status
+ECHO -
+ECHO Please wait... [up to 1 hour]
+
 CALL "C:\Program Files\R\R-3.4.2\bin\x64\RScript.exe" "R\working_directory.R"
 
-CALL "C:\Program Files\R\R-3.4.2\bin\x64\RScript.exe" "R\booking.R"
+if not exist "log\" mkdir log\
+
+CALL "C:\Program Files\R\R-3.4.2\bin\x64\RScript.exe" "R\booking.R" > log\log_%DATE:~-4%-%DATE:~4,2%-%DATE:~7,2%-%TIME:~0,2%h%TIME:~3,2%m.txt 2>&1
 
 PAUSE
 
